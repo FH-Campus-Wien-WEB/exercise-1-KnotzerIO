37b6920703ec5c5f487fcaa79b52ebd29f18b788
Mark <knotzermark@gmail.com>
2026-04-12 18:54:59 +0200

    implement movie endpoint

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const fs = require('fs');
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+  fs.readFile(path.join(__dirname, 'data', 'movies.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading movies data');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
 })
 
 app.listen(3000)
